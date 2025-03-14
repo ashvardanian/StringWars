65cc43ded011b1afa00554cb8c191b9228458561
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2025-03-14 18:18:45 +0000

    Add: Byteset search benchmarks

--- bench_find.rs
+++ bench_find.rs
@@ -1,9 +1,17 @@
 //! # StringWa.rs: Substring Search Benchmarks
 //!
-//! This file benchmarks the forward and reverse exact substring search functionality provided by
+//! This file benchmarks the forward and backward exact substring search functionality provided by
 //! the StringZilla library and the memchr crate. The input file is treated as a haystack and all
 //! of its tokens as needles. The throughput numbers are reported in Gigabytes per Second and for
 //! any sampled token - all of its inclusions in a string are located.
+//!
+//! The input file is treated as a haystack and all of its tokens as needles. For substring searches,
+//! each occurrence is located. For byteset searches, three separate operations are performed per token,
+//! looking for:
+//!
+//! - any of "\n\r\v\f" - the 4 tabulation characters
+//! - any of "</>&'\"=[]" - the 9 HTML-related characters
+//! - any of "0123456789" - the 10 numeric characters
 //!
 //! ## Usage Examples
 //!
@@ -28,8 +36,16 @@ use std::time::Duration;
 
 use criterion::{black_box, Criterion, Throughput};
 
+use aho_corasick::AhoCorasick;
+use bstr::ByteSlice;
 use memchr::memmem;
-use stringzilla::sz::{find as sz_find, rfind as sz_rfind};
+use regex::bytes::Regex;
+use stringzilla::sz::{
+    find as sz_find,
+    find_byteset as sz_find_byteset, //
+    rfind as sz_rfind,
+    Byteset,
+};
 
 use stringzilla::sz::{
     // Pull some metadata logging functionality
@@ -77,19 +93,25 @@ fn bench_find(c: &mut Criterion) {
     let haystack_length = haystack.len();
 
     // Benchmarks for forward search
-    let mut g = c.benchmark_group("search-forward");
+    let mut g = c.benchmark_group("substring-forward");
     g.throughput(Throughput::Bytes(haystack_length as u64));
-    perform_forward_benchmarks(&mut g, &needles, haystack);
+    bench_substring_forward(&mut g, &needles, haystack);
     g.finish();
 
-    // Benchmarks for reverse search
-    let mut g = c.benchmark_group("search-reverse");
+    // Benchmarks for backward search
+    let mut g = c.benchmark_group("substring-backward");
     g.throughput(Throughput::Bytes(haystack_length as u64));
-    perform_reverse_benchmarks(&mut g, &needles, haystack);
+    bench_substring_backward(&mut g, &needles, haystack);
+    g.finish();
+
+    // Benchmarks for byteset search
+    let mut g = c.benchmark_group("byteset-forward");
+    g.throughput(Throughput::Bytes(3 * haystack_length as u64));
+    bench_byteset_forward(&mut g, &needles);
     g.finish();
 }
 
-fn perform_forward_benchmarks(
+fn bench_substring_forward(
     g: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
     needles: &[&str],
     haystack: &[u8],
@@ -126,19 +148,19 @@ fn perform_forward_benchmarks(
         b.iter(|| {
             let token = black_box(*tokens.next().unwrap());
             let mut pos = 0;
-            while let Some(found) = haystack_str[pos..].find(token) {
+            while let Some(found) = haystack[pos..].find(token) {
                 pos += found + token.len();
             }
         })
     });
 }
 
-fn perform_reverse_benchmarks(
+fn bench_substring_backward(
     g: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
     needles: &[&str],
     haystack: &[u8],
 ) {
-    // Benchmark for StringZilla reverse search using a cycle iterator.
+    // Benchmark for StringZilla backward search using a cycle iterator.
     let mut tokens = needles.iter().cycle();
     g.bench_function("sz::rfind", |b| {
         b.iter(|| {
@@ -155,7 +177,7 @@ fn perform_reverse_benchmarks(
         })
     });
 
-    // Benchmark for memmem reverse search using a cycle iterator.
+    // Benchmark for memmem backward search using a cycle iterator.
     let mut tokens = needles.iter().cycle();
     g.bench_function("memmem::rfind", |b| {
         b.iter(|| {
@@ -172,19 +194,132 @@ fn perform_reverse_benchmarks(
         })
     });
 
-    // Benchmark for default `std::str` reverse search.
+    // Benchmark for default `std::str` backward search.
     let mut tokens = needles.iter().cycle();
     g.bench_function("std::str::rfind", |b| {
         b.iter(|| {
             let token = black_box(*tokens.next().unwrap());
-            let mut pos: Option<usize> = Some(haystack_str.len());
+            let mut pos: Option<usize> = Some(haystack.len());
             while let Some(end) = pos {
-                if let Some(found) = haystack_str[..end].rfind(token) {
+                if let Some(found) = haystack[..end].rfind(token) {
                     pos = Some(found);
                 } else {
                     break;
                 }
             }
+        })
+    });
+}
+
+fn bench_byteset_forward(
+    g: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
+    needles: &[&str],
+) {
+    // Define the three bytesets we will analyze.
+    const BYTES_TABS: &[u8] = b"\n\r\x0B\x0C";
+    const BYTES_HTML: &[u8] = b"</>&'\"=[]";
+    const BYTES_DIGITS: &[u8] = b"0123456789";
+
+    // Benchmark for StringZilla forward search using a cycle iterator.
+    let mut tokens = needles.iter().cycle();
+    let sz_tabs = Byteset::from(BYTES_TABS);
+    let sz_html = Byteset::from(BYTES_HTML);
+    let sz_digits = Byteset::from(BYTES_DIGITS);
+    g.bench_function("sz::find_byteset", |b| {
+        b.iter(|| {
+            let token = black_box(*tokens.next().unwrap());
+            let token_bytes = black_box(token.as_bytes());
+            let mut pos: usize = 0;
+            while let Some(found) = sz_find_byteset(&token_bytes[pos..], sz_tabs) {
+                pos += found + 1;
+            }
+            pos = 0;
+            while let Some(found) = sz_find_byteset(&token_bytes[pos..], sz_html) {
+                pos += found + 1;
+            }
+            pos = 0;
+            while let Some(found) = sz_find_byteset(&token_bytes[pos..], sz_digits) {
+                pos += found + 1;
+            }
+        })
+    });
+
+    // Benchmark for bstr's byteset search.
+    let mut tokens = needles.iter().cycle();
+    g.bench_function("bstr::iter", |b| {
+        b.iter(|| {
+            let token = black_box(*tokens.next().unwrap());
+            let token_bytes = black_box(token.as_bytes());
+            let mut pos: usize = 0;
+            // Inline search for `BYTES_TABS`.
+            while let Some(found) = token_bytes[pos..]
+                .iter()
+                .position(|&c| BYTES_TABS.contains(&c))
+            {
+                pos += found + 1;
+            }
+            pos = 0;
+            // Inline search for `BYTES_HTML`.
+            while let Some(found) = token_bytes[pos..]
+                .iter()
+                .position(|&c| BYTES_HTML.contains(&c))
+            {
+                pos += found + 1;
+            }
+            pos = 0;
+            // Inline search for `BYTES_DIGITS`.
+            while let Some(found) = token_bytes[pos..]
+                .iter()
+                .position(|&c| BYTES_DIGITS.contains(&c))
+            {
+                pos += found + 1;
+            }
+        })
+    });
+
+    // Benchmark for Regex-based byteset search.
+    let mut tokens = needles.iter().cycle();
+    let re_tabs = Regex::new("[\n\r\x0B\x0C]").unwrap();
+    let re_html = Regex::new("[</>&'\"=\\[\\]]").unwrap();
+    let re_digits = Regex::new("[0-9]").unwrap();
+    g.bench_function("regex::find_iter", |b| {
+        b.iter(|| {
+            let token = black_box(*tokens.next().unwrap());
+            black_box(re_tabs.find_iter(token.as_bytes()).count());
+            black_box(re_html.find_iter(token.as_bytes()).count());
+            black_box(re_digits.find_iter(token.as_bytes()).count());
+        })
+    });
+
+    // Benchmark for Aho–Corasick-based byteset search.
+    let mut tokens: std::iter::Cycle<std::slice::Iter<'_, &str>> = needles.iter().cycle();
+    let ac_tabs = AhoCorasick::new(
+        &BYTES_TABS
+            .iter()
+            .map(|&b| (b as char).to_string())
+            .collect::<Vec<_>>(),
+    )
+    .expect("failed to create AhoCorasick FSA");
+    let ac_html = AhoCorasick::new(
+        &BYTES_HTML
+            .iter()
+            .map(|&b| (b as char).to_string())
+            .collect::<Vec<_>>(),
+    )
+    .expect("failed to create AhoCorasick FSA");
+    let ac_digits = AhoCorasick::new(
+        &BYTES_DIGITS
+            .iter()
+            .map(|&b| (b as char).to_string())
+            .collect::<Vec<_>>(),
+    )
+    .expect("failed to create AhoCorasick FSA");
+    g.bench_function("aho_corasick::find_iter", |b| {
+        b.iter(|| {
+            let token = black_box(*tokens.next().unwrap());
+            black_box(ac_tabs.find_iter(token).count());
+            black_box(ac_html.find_iter(token).count());
+            black_box(ac_digits.find_iter(token).count());
         })
     });
 }
